e041040cc17d441e6b88295376cd67bcb58bfcdb
Radosław Górski <26116022+Gadek@users.noreply.github.com>
2022-04-01 00:45:54 +0200

    Update generator_esxi_logs.py

--- generator_esxi_logs.py
+++ generator_esxi_logs.py
@@ -4,6 +4,8 @@ from datetime import datetime
 import logging
 
 logging.basicConfig(format='%(message)s', filename='/var/log/esxi.log', level=logging.DEBUG, encoding='utf-8')
+print("This script generates logs in /var/log/esxi.log which imitate failed login attempts. They are in format:")
+print("2022-04-01 00:41:48 host6 Hostd: warning hostd[526387] [Originator@6876 sub=Default opID=esxui-9558-3741] Rejected password for user user-user5 from 10.16.89.3")
 host_list = ["host", "host-h2", "host-ho3", "host-hos4", "host-host5", "host6", "host7", "host8", "host9", "host10"]
 user_list = ["user1", "user-u2", "user-us3", "user-use4","user-user5"]
 ip_list = ["10.129.210.1", "10.14.34.2", "10.16.89.3", "10.14.199.4","10.145.99.5"]
