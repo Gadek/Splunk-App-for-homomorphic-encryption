258277e5313a0b4a2d7385c27559a223c2e91681
Pawel Hanusik <39835401+pawelhanusik@users.noreply.github.com>
2022-06-16 19:51:20 +0200

    Add FindMaliciousHashesOperation

---
+++ src/operations/FindMaliciousHashesOperation.py
@@ -0,0 +1,72 @@
+import numpy as np
+
+from src.operations.Operation import Operation, OperationResult
+import src.Utils as Utils
+import sys
+
+class FindMaliciousHashesResult(OperationResult):
+    def __init__(self, result):
+        self.result = result
+    
+    def decrypt(self, HE):
+        decryptedResult = {}
+
+        for encryptedHash in self.result:
+            encryptedComparison = self.result[encryptedHash]
+
+            decryptedHash = Utils.number2string(
+                Utils.getNumberFromSplittedInto15bits(
+                    HE.decrypt(encryptedHash)
+                )
+            )
+            decryptedComparison = Utils.getNumberFromSplittedInto15bits(
+                HE.decrypt(encryptedComparison)
+            )
+
+            decryptedResult[decryptedHash] = (decryptedComparison == 0)
+        
+        self.result = decryptedResult
+
+    def __str__(self):
+        ret = ''
+
+        for hash in self.result:
+            comparison = self.result[hash]
+
+            ret += hash + '\t' + str(comparison) + '\n'
+        
+
+        return ret
+
+class FindMaliciousHashesOperation(Operation):
+    def __init__(self, hashes = [], malicious =[]):
+        self.hashes = hashes
+        self.malicious = malicious
+
+    def encrypt(self, HE):
+        for i in range(len(self.hashes)):
+            self.hashes[i] = HE.encrypt(
+                Utils.splitNumberInto15bits(
+                    Utils.string2number(self.hashes[i])
+                )
+            )
+        
+        for i in range(len(self.malicious)):
+            self.malicious[i] = HE.encrypt(
+                Utils.splitNumberInto15bits(
+                    Utils.string2number(self.malicious[i])
+                )
+            )
+
+    def run(self) -> FindMaliciousHashesResult:
+        ret = {}
+
+        for s in self.hashes:
+            comparison = 1
+
+            for t in self.malicious:
+                comparison *= (s - t)
+            
+            ret[s] = comparison
+        
+        return FindMaliciousHashesResult(ret)
