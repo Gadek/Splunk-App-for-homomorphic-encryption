5a2d80186f75e12105b0d1fa61d636ec128405f6
Paweł Hanusik <39835401+pawelhanusik@users.noreply.github.com>
2022-09-13 22:45:54 +0200

    Add IpGroupAndCountOperation

--- he_test.py
+++ he_test.py
@@ -11,7 +11,7 @@ import src.Utils as Utils
 from src.operations.AddNumbersOperation import AddNumbersOperation
 from src.operations.AreStringsPresentInTableOperation import AreStringsPresentInTableOperation
 from src.operations.FindMaliciousHashesOperation import FindMaliciousHashesOperation
-from src.operations.GroupAndCountOperation import GroupAndCountOperation
+from src.operations.IpGroupAndCountOperation import IpGroupAndCountOperation
 
 def getHEContext(loadSaveFromFS = True):
     if loadSaveFromFS and os.path.exists('HE_context_and_keys'):
@@ -42,36 +42,36 @@ print(f"n={n}\nsec={sec}\nt={t}\nmax_bits={max_bits}\n")
 # | Operation example - add numbers |
 # ===================================
 
-# print("=" * 20)
-# print("Operation example - add numbers")
-# print("-" * 20)
+print("=" * 20)
+print("Operation example - add numbers")
+print("-" * 20)
 
-# operation = AddNumbersOperation([127, -2, -2, -2, -1])
-# operation.encrypt(HE)
-# res = operation.run()
-# res.decrypt(HE)
+operation = AddNumbersOperation([127, -2, -2, -2, -1])
+operation.encrypt(HE)
+res = operation.run()
+res.decrypt(HE)
 
-# print(str(res))
-# print("-" * 20)
+print(str(res))
+print("-" * 20)
 
 # ===================================
 # | Operation example - hash search |
 # ===================================
 
-# print("=" * 20)
-# print("Operation example - hash search")
-# print("-" * 20)
+print("=" * 20)
+print("Operation example - hash search")
+print("-" * 20)
 
-# operation = FindMaliciousHashesOperation([
-#     '11ba3e87ec5e20a2d41063696b27ece12d644bd32892f33464d5d62ca9be492f',
-#     '11ba3e87ec5e20a2d41063696b27ece12d644bd32892f33464d5d62ca9be492e'
-# ])
-# operation.encrypt(HE)
-# res = operation.run()
-# res.decrypt(HE)
+operation = FindMaliciousHashesOperation([
+    '11ba3e87ec5e20a2d41063696b27ece12d644bd32892f33464d5d62ca9be492f',
+    '11ba3e87ec5e20a2d41063696b27ece12d644bd32892f33464d5d62ca9be492e'
+])
+operation.encrypt(HE)
+res = operation.run()
+res.decrypt(HE)
 
-# print(str(res))
-# print("-" * 20)
+print(str(res))
+print("-" * 20)
 
 # =====================================
 # | Operation example - group & count |
@@ -81,60 +81,14 @@ print("=" * 20)
 print("Operation example - group & count")
 print("-" * 20)
 
-"""
-195 => 6
-127 => 4
-1   => 3
-2   => 1
-3   => 2
-100 => 3
-"""
-"""
-operation = GroupAndCountOperation([
-    195,
-    195,
-    127,
-    195,
-    127,
-    195,
-    1,
-    195,
-    127,
-    127,
-    195,
-    2,
-    3,
-    100,
-    1,
-    3,
-    1,
-    100,
-    100,
-])
-"""
-
-def ip2int(ip):
-    ip = ip.split('.')
-
-    if len(ip) > 2:
-        raise Exception("Only 2 octets of IP are supported!")
-
-    ret = 0
-
-    for x in ip:
-        ret << 8
-        ret |= int(x)
-
-    return ret
-
-operation = GroupAndCountOperation([
-    ip2int('0.156'),
-    ip2int('0.156'),
-    ip2int('255.199'),
-    ip2int('0.156'),
-    ip2int('255.199'),
-    ip2int('0.199'),
-    ip2int('0.199'),
+operation = IpGroupAndCountOperation([
+    '10.0.0.156',
+    '10.0.0.156',
+    '10.0.255.199',
+    '10.0.0.156',
+    '10.0.255.199',
+    '10.0.0.199',
+    '10.0.0.199',
 ])
 operation.encrypt(HE)
 res = operation.run()
---
+++ src/operations/IpGroupAndCountOperation.py
@@ -0,0 +1,114 @@
+import numpy as np
+
+from src.operations.GroupAndCountOperation import GroupAndCountOperation, GroupAndCountResult
+import src.Utils as Utils
+
+class IpGroupAndCountResult(GroupAndCountResult):
+    def __init__(self, prefix, result):
+        self.prefix = prefix
+        super().__init__(result)
+
+    def decrypt(self, HE):
+        max_bits = Utils.getHEMaxBits(HE)
+        
+        decryptedPrefix = Utils.number2string(
+            Utils.arrayIntoNumber(
+                HE.decrypt(self.prefix),
+                max_bits
+            )
+        )
+
+        super().decrypt(HE)
+
+        for ip_int in self.result:
+            ip_str = self.__int2ip(ip_int)
+            ip_str = f'{decryptedPrefix}.{ip_str}'
+            self.result[ip_str] = self.result.pop(ip_int)
+
+    def __int2ip(self, i):
+        parts = []
+        
+        i = np.array([i], dtype=np.ushort)[0]
+
+        while i > 0:
+            parts += [str(i & 0xff)]
+            i >>= 8
+        
+        return '.'.join(reversed(parts))
+
+class IpGroupAndCountOperation(GroupAndCountOperation):
+    def __init__(self, data = []):
+        self.prefix = self.__getPrefix(data)
+
+        if self.prefix is None:
+            raise Exception("IP addresses have to have the same first 2 octets!")
+
+        super().__init__(
+            list(
+                map(
+                    lambda x: self.__ip2int(
+                        self.__removePrefixFromIP(x)
+                    ),
+                    data
+                )
+            )
+        )
+    
+    def __getPrefix(self, data):
+        prefix = None
+
+        for currIP in data:
+            currIPPrefix = self.__splitIP(currIP)[0]
+
+            if prefix is None:
+                prefix = currIPPrefix
+            else:
+                if currIPPrefix != prefix:
+                    return None
+        
+        return prefix
+    
+    def __removePrefixFromIP(self, ip):
+        return self.__splitIP(ip)[1]
+    
+    def __splitIP(self, ip):
+        ipSegments = ip.split('.')
+
+        if len(ipSegments) != 4:
+            raise Exception(f"IP have to have 4 octets! Invlaid IP: {ip}")
+        
+        return [
+            f'{ipSegments[0]}.{ipSegments[1]}',
+            f'{ipSegments[2]}.{ipSegments[3]}'
+        ]
+    
+    def __ip2int(self, ip):
+        ip = ip.split('.')
+
+        if len(ip) > 2:
+            raise Exception("Only 2 octets of IP are supported!")
+
+        ret = 0
+
+        for x in ip:
+            ret <<= 8
+            ret |= int(x)
+
+        return ret
+    
+    def encrypt(self, HE):
+        max_bits = Utils.getHEMaxBits(HE)
+
+        self.prefix = HE.encrypt(
+            Utils.numberIntoArray(
+                Utils.string2number(self.prefix),
+                max_bits
+            )
+        )
+
+        super().encrypt(HE)
+    
+    def run(self) -> IpGroupAndCountResult:
+        res = super().run()
+
+        return IpGroupAndCountResult(self.prefix, res.result)
