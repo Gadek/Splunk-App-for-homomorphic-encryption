ad441b067e7c9e07886267659d9d799b10eb6415
Radosław Górski <26116022+Gadek@users.noreply.github.com>
2022-09-14 21:26:42 +0200

    Update hash_report.py
    Added authorization via token.
    Script gets arguments now.

--- hash_report.py
+++ hash_report.py
@@ -2,6 +2,7 @@ import splunklib.client as client
 import splunklib.results as results
 from dotenv import load_dotenv
 import os
+import sys
 from time import sleep
 import pickle
 
@@ -29,14 +30,17 @@ from socket_utils import process
 #     "offset", "count", "search", "field_list", "f", "output_mode"
 # ]
 
-search='search index=hashes | head 5'
-create_dict = dict()
-create_dict['rf'] = "hash"
-create_dict['earliest_time'] = "-1m"
-results_dict = dict()
-results_dict['count'] = 0
-results_dict['output_mode'] = "json"
-results_dict['field_list'] = "_raw hash"
+
+
+def __shorten_hashes(hashes):
+    short_hashes = []
+
+    for h in hashes:
+        short_hashes += [
+            h[:]
+        ]
+
+    return short_hashe
 
 def __getHEContext():
     if os.path.exists('HE_context_and_keys'):
@@ -46,33 +50,42 @@ def __getHEContext():
     HE = Pyfhel()
     HE.contextGen(scheme='bfv', n=2**15, t_bits=34)
     HE.keyGen()
-    
+
     PyfhelUtils.saveHE('HE_context_and_keys', HE)
 
     return HE
 
-def run_job(service):
-    search_result = get_hashes(service)
-    
+def run_job(service, idx, earliest_time, latest_time):
+    search = 'search index=' + idx + ' | head 5'
+    create_dict = dict()
+    create_dict['rf'] = "hash"
+    create_dict['earliest_time'] = earliest_time
+    create_dict['latest_time'] = latest_time
+    results_dict = dict()
+    results_dict['count'] = 0
+    results_dict['output_mode'] = "json"
+    results_dict['field_list'] = "_raw hash"
+    search_result = get_hashes(service, search, create_dict, results_dict)
+
     HE = __getHEContext()
 
     operation = prepare_operation(HE, search_result)
     received = process(pickle.dumps(operation))      # send prepared operation to processor
     res = pickle.loads(received)
 
-    ########## replaced with sockets #############
+    # ######### replaced with sockets #############
     # FileIO.savePickle('fileA.pickle', operation)
     # print("Please run processor with input file=fileA.pickle and output file=fileB.pickle")
     # print("Then press ENTER")
     # tmp = input()
-
+    #
     # res = FileIO.loadPickle('fileB.pickle')
-    ##############################################
+    # #############################################
     result_logs = decrypt_result(HE, res)
-    
+
     send_result_hashes(service, result_logs)
 
-def get_hashes(service):
+def get_hashes(service, search, create_dict, results_dict):
     job = service.jobs.create(search,**create_dict)
 
     while True:
@@ -91,13 +104,13 @@ def get_hashes(service):
 
 def prepare_operation(HE, search_result):
     hashes_to_check = []
-    
+
     for result in search_result:
         print("search res:", result)
         hashes_to_check += [
             result['hash']
         ]
-    
+
     operation = FindMaliciousHashesOperation(hashes_to_check)
     operation.encrypt(HE)
 
@@ -118,17 +131,23 @@ def send_result_hashes(service, result_logs):
 
 
 def main():
-    load_dotenv()
+    # load_dotenv()
 
     host = os.getenv('host')
     port = os.getenv('port')
-    username = os.getenv('username')
-    password = os.getenv('password')
-
-    service = client.connect(host=host, port=port, username=username, password=password)
+    # username = os.getenv('username')
+    # password = os.getenv('password')
+    token = os.getenv('token')
+    service = client.connect(host=host, port=port, token=token)
     assert isinstance(service, client.Service)
 
-    run_job(service)
+    idx = sys.argv[1]
+    #dest_idx = sys.argv[2]
+    time1 = sys.argv[3]
+    time2 = sys.argv[4]
+    #service = sys.argv[5]
+
+    run_job(service, idx, time1, time2)
 
 
 if __name__ == "__main__":
