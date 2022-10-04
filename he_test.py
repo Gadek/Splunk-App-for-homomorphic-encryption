43e4d98b02078aa5cb1d207e11fb57c7302164fa
Pawel Hanusik <39835401+pawelhanusik@users.noreply.github.com>
2022-10-04 23:52:36 +0200

    Update he_test.py

--- he_test.py
+++ he_test.py
@@ -12,10 +12,11 @@ from src.operations.AddNumbersOperation import AddNumbersOperation
 from src.operations.AreStringsPresentInTableOperation import AreStringsPresentInTableOperation
 from src.operations.FindMaliciousHashesOperation import FindMaliciousHashesOperation
 from src.operations.IpGroupAndCountOperation import IpGroupAndCountOperation
+from src.operations.GroupAndCountOperation import GroupAndCountOperation
 
 def getHEContext(loadSaveFromFS = True):
-    if loadSaveFromFS and os.path.exists('HE_context_and_keys'):
-        HE = PyfhelUtils.loadHE('HE_context_and_keys')
+    if loadSaveFromFS and os.path.exists('HE_context_and_keys/he_test'):
+        HE = PyfhelUtils.loadHE('HE_context_and_keys/he_test')
         return HE
 
     HE = Pyfhel()
@@ -26,7 +27,7 @@ def getHEContext(loadSaveFromFS = True):
     HE.rotateKeyGen()
     
     if loadSaveFromFS:
-        PyfhelUtils.saveHE('HE_context_and_keys', HE, True, True)
+        PyfhelUtils.saveHE('HE_context_and_keys/he_test', HE, True, True)
 
     return HE
 
@@ -64,7 +65,14 @@ print("-" * 20)
 
 operation = FindMaliciousHashesOperation([
     '11ba3e87ec5e20a2d41063696b27ece12d644bd32892f33464d5d62ca9be492f',
-    '11ba3e87ec5e20a2d41063696b27ece12d644bd32892f33464d5d62ca9be492e'
+    '11ba3e87ec5e20a2d41063696b27ece12d644bd32892f33464d5d62ca9be492e',
+    '11ba3e87ec5e20a2d41063696b27ece12d644bd32892f33464d5d62ca9be492e',
+    '11ba3e87ec5e20a2d41063696b27ece12d644bd32892f33464d5d62ca9be492f',
+    '11ba3e87ec5e20a2d41063696b27ece12d644bd32892f33464d5d62ca9be492e',
+    '11ba3e87ec5e20a2d41063696b27ece12d644bd32892f33464d5d62ca9be492e',
+    '4740284572d290ecbf865e6fa0c63168cff906038db7a2fa7181509360cd3f3b',
+    '4740284572d290ecbf865e6fa0c63168cff906038db7a2fa7181509360c43f3b',
+    '4760284572d290ecbf865e6fa0c63168cff906038db7a2fa7181509360cd3f3b',
 ])
 operation.encrypt(HE)
 res = operation.run()
